983e77d14127e4494c351ee02b08c45ce351d224
watermode <smatkotimur@gmail.com>
2025-04-30 09:25:31 +0300

    ex1.js

---
+++ ex1.js
@@ -0,0 +1,9 @@
+function ucFirst(str) {
+    if (!str) return str;
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  
+  console.log(ucFirst("hello"));
+  console.log(ucFirst("world"));
+  console.log(ucFirst("")); 
+  
